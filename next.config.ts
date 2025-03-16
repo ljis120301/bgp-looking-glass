77bca9b2f4e2e99d95696b495b6a5ee121464496
Jason <you@example.com>
2025-03-16 14:14:13 -0700

    .

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: true,
-  },
-  // Set production port to 3090
-  env: {
-    PORT: process.env.NODE_ENV === 'production' ? '3090' : '3000'
-  }
 };
 
 export default nextConfig;
